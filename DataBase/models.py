84387a6a1b6c029a4099022a878c802a81b72d6c
Kirienko-Alyona <ladybird.miami@gmail.com>
2023-02-28 02:42:13 +0200

    changed

--- DataBase/models.py
+++ DataBase/models.py
@@ -9,41 +9,44 @@ class Team(Base):
     __tablename__ = "teams"
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(150), nullable=False, unique=True)
-    
-    
+
+
 class Student(Base):
     __tablename__ = "students"
     id = Column(Integer, primary_key=True, nullable=False)
     fullname = Column(String(250), nullable=False, unique=True)
     team_id = Column(Integer, ForeignKey("teams.id", ondelete="CASCADE"))
-    
+
     teams_rel = relationship("Team", backref="students")
-    
-    
+
+
 class Teacher(Base):
     __tablename__ = "teachers"
     id = Column(Integer, primary_key=True, nullable=False)
     fullname = Column(String(250), nullable=False, unique=True)
-    
+
 
 class Discipline(Base):
     __tablename__ = "disciplines"
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(50), nullable=False)
-    teacher_id = Column("teacher_id", ForeignKey('teachers.id', ondelete="CASCADE"))
-    
+    teacher_id = Column("teacher_id", ForeignKey(
+        'teachers.id', ondelete="CASCADE"))
+
     teachers_rel = relationship("Teacher", backref="disciplines")
-    
-    
+
+
 class Grade(Base):
-    __tablename__ = "grades" 
+    __tablename__ = "grades"
     id = Column(Integer, primary_key=True, nullable=False)
     grade = Column(Integer, primary_key=True, nullable=False)
-    discipline_id = Column("discipline_id", ForeignKey('disciplines.id', ondelete="CASCADE"))
-    student_id = Column("student_id", ForeignKey('students.id', ondelete="CASCADE"))
-    date_of = Column(Date, primary_key=True, default=func.now(), nullable=False)
-    
-    discipline = relationship("Discipline", backref="grades", passive_deletes=True)
+    discipline_id = Column("discipline_id", ForeignKey(
+        'disciplines.id', ondelete="CASCADE"))
+    student_id = Column("student_id", ForeignKey(
+        'students.id', ondelete="CASCADE"))
+    date_of = Column(Date, primary_key=True,
+                     default=func.now(), nullable=False)
+
+    discipline = relationship(
+        "Discipline", backref="grades", passive_deletes=True)
     students = relationship("Student", backref="grades", passive_deletes=True)
-    
-    
--- DataBase/seeds.py
+++ DataBase/seeds.py
@@ -19,59 +19,64 @@ NUMBER_TEACHERS = 5
 NUMBER_STUDENTS = 50
 fake = Faker("uk_UA")
 
+
 def create_teams():
     for team_ in TEAMS:
         team = Team(
-            name = team_
-    )
+            name=team_
+        )
         session.add(team)
-    session.commit() 
-    
+    session.commit()
+
+
 def create_students():
-    teams_rel = session.query(Team.id).all() 
+    teams_rel = session.query(Team.id).all()
     for _ in range(1, NUMBER_STUDENTS + 1):
         student = Student(
-            fullname = fake.name(),
-            team_id = (random.choice(teams_rel))[0]
+            fullname=fake.name(),
+            team_id=(random.choice(teams_rel))[0]
         )
         session.add(student)
-    session.commit() 
+    session.commit()
+
 
 def create_teachers():
     for _ in range(1, NUMBER_TEACHERS + 1):
         teacher = Teacher(
-            fullname = fake.name()
+            fullname=fake.name()
         )
         session.add(teacher)
-    session.commit() 
-    
+    session.commit()
+
+
 def create_disciplines():
     teachers_rel = session.query(Teacher.id).all()
     print(teachers_rel)
-    
+
     for discip in DISCIPLINES:
         discipline = Discipline(
-            name = discip,
-            teacher_id = (random.choice(teachers_rel))[0]
-        )   
+            name=discip,
+            teacher_id=(random.choice(teachers_rel))[0]
+        )
         session.add(discipline)
-    session.commit()             
-    
+    session.commit()
+
+
 def create_grades():
     disciplines_rel = session.query(Discipline.id).all()
     students_rel = session.query(Student.id).all()
     for i in range(1, NUMBER_STUDENTS + 1):
         grade = Grade(
-            grade = random.choice(range(1, 12)),
-            date_of = fake.date_between(start_date="-1y"),
-            discipline_id = (random.choice(disciplines_rel))[0],
-            student_id = (random.choice(students_rel))[0],
-            id = i
+            grade=random.choice(range(1, 12)),
+            date_of=fake.date_between(start_date="-1y"),
+            discipline_id=(random.choice(disciplines_rel))[0],
+            student_id=(random.choice(students_rel))[0],
+            id=i
         )
         session.add(grade)
-    session.commit()  
+    session.commit()
 
-    
+
 if __name__ == '__main__':
     create_teams()
     create_students()
